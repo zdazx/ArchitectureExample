38a63941f7e11a663f7825ecb989595887d3eb62
zdazx <zhangduo0227@163.com>
2020-07-28 19:52:21 +0800

    implment view model to increase

---
+++ app/src/main/java/com/thoughtworks/architectureexample/ArchViewModel.java
@@ -0,0 +1,68 @@
+package com.thoughtworks.architectureexample;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class ArchViewModel extends ViewModel {
+    private MutableLiveData<Integer> number;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private Disposable disposable;
+
+    public LiveData<Integer> getNumber() {
+        if (Objects.isNull(number)) {
+            number = new MutableLiveData<>();
+        }
+        return number;
+    }
+
+    public void increase() {
+        if (Objects.nonNull(disposable) && !disposable.isDisposed()) {
+            return;
+        }
+        Observable.interval(1, TimeUnit.SECONDS)
+                .map(Long::intValue)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable = d;
+                        compositeDisposable.add(disposable);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        number.setValue(integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onCleared() {
+        if (!compositeDisposable.isDisposed()) {
+            compositeDisposable.clear();
+        }
+        super.onCleared();
+    }
+}
--- app/src/main/java/com/thoughtworks/architectureexample/MainActivity.java
+++ app/src/main/java/com/thoughtworks/architectureexample/MainActivity.java
@@ -2,21 +2,30 @@ package com.thoughtworks.architectureexample;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
+    private ArchViewModel archViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView textView = findViewById(R.id.increase_text_view);
         Button increaseBtn = findViewById(R.id.increase_btn);
         increaseBtn.setOnClickListener(view -> startIncrease());
+
+        archViewModel = new ViewModelProvider(this).get(ArchViewModel.class);
+        final Observer<Integer> observer = integer -> textView.setText(String.valueOf(integer));
+        archViewModel.getNumber().observe(this, observer);
     }
 
     private void startIncrease() {
-
+        archViewModel.increase();
     }
 }
